103cb70f9c987afb7e72885c76c50f457f4bb8a4
Andreas Skovsted <as.kristiansen10@hotmail.com>
2026-01-05 17:18:56 +0100

    added latest logs because of azure git ignore removed it

---
+++ av-monitoring-system-backend/Functions/Logs/GetLogs.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using AVMonitoring.Functions.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace AVMonitoring.Functions.Functions.Logs
+{
+    public class GetLogs
+    {
+        private readonly MonitoringLogRepository _logs;
+
+        public GetLogs(MonitoringLogRepository logs)
+        {
+            _logs = logs;
+        }
+
+        [Function("GetLogs")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")]
+            HttpRequestData req)
+        {
+            var data = await _logs.GetAllAsync();
+
+            var res = req.CreateResponse(HttpStatusCode.OK);
+            await res.WriteAsJsonAsync(data);
+            return res;
+        }
+    }
+}
